152eedf9b2c34f8b839cf7b2c84fda25db56471e
Gabriel Ostrolucký <gabriel.ostrolucky@gmail.com>
2021-12-29 10:03:41 +0100

    d

--- Tests/DependencyInjection/ConfigurationTest.php
+++ Tests/DependencyInjection/ConfigurationTest.php
@@ -3,7 +3,6 @@
 namespace Doctrine\Bundle\DoctrineBundle\Tests\DependencyInjection;
 
 use Doctrine\Bundle\DoctrineBundle\DependencyInjection\Configuration;
-use Doctrine\Common\Proxy\AbstractProxyFactory;
 use PHPUnit\Framework\TestCase;
 use function class_exists;
 
@@ -25,6 +24,6 @@ class ConfigurationTest extends TestCase
     {
         $configuration = new Configuration(true);
         $configuration->getConfigTreeBuilder();
-        class_exists(AbstractProxyFactory::class, false);
+        $this->assertFalse(class_exists('Doctrine\Common\Proxy\AbstractProxyFactory', false));
     }
 }

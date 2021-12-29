020857986a0797e447217b8b77754b73246fcb06
Gabriel Ostrolucký <gabriel.ostrolucky@gmail.com>
2021-12-29 09:54:41 +0100

    b

--- Tests/DependencyInjection/ConfigurationTest.php
+++ Tests/DependencyInjection/ConfigurationTest.php
@@ -22,5 +22,8 @@ class ConfigurationTest extends TestCase
     /** @runInSeparateProcess */
     public function testGetConfigTreeBuilderDoNotUseDoctrineCommon(): void
     {
+        $configuration = new Configuration(true);
+        $configuration->getConfigTreeBuilder();
+        $this->assertFalse(class_exists('Doctrine\Common\Proxy\AbstractProxyFactory', false));
     }
 }
